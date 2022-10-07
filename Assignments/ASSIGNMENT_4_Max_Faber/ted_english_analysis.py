b68186b952e9b46846a6059041ff44a17caa8d8b
maxfaber <maxfaber@quicknet.nl>
2022-10-07 22:42:43 +0200

    Progress on assignment 4b

--- Assignments/ASSIGNMENT_4_Max_Faber/ted_english_analysis.py
+++ Assignments/ASSIGNMENT_4_Max_Faber/ted_english_analysis.py
@@ -1,56 +1,74 @@
-import nltk
-
-from lxml import etree as et
-from utils import find_shortest_talk, find_longest_talk, find_mean_talk_length
+from utils import load_root, get_talks, get_word_count_overview, get_mean_talk_length, get_datetime_overview
 
 
-def find_wc(talks, length='shortest'):
+def find_wc(talks, length='longest'):
 	"""
 
 	:param talks:
 	:param length:
 	:return:
 	"""
-	keyword_shortest = 'shortest'
 	keyword_longest = 'longest'
-	keyword_mean = 'mean'
-	result = ('', '', -1)
+	keyword_shortest = 'shortest'
+	assert length in [keyword_longest, keyword_shortest],\
+		f"Please specify a valid length requirement, possible options are '{keyword_longest}' and '{keyword_shortest}'!"
+	word_count_overview = get_word_count_overview(talks)
+	mean_talk_length = get_mean_talk_length(word_count_overview)
+	mean_talk_length = int(mean_talk_length)
 
-	assert(length in [keyword_shortest, keyword_longest, keyword_mean], f"Please specify a valid length requirement, possible options are '{keyword_shortest}', '{keyword_longest}' and '{keyword_mean}!'")
-	if length == 'shortest':
-		result = find_shortest_talk(talks)
-	elif length == 'longest':
-		result = find_longest_talk(talks)
-	elif length == 'mean':
-		result = find_mean_talk_length()
-	return result
+	if length == 'longest':
+		talk_overview = max(word_count_overview, key=lambda talk: talk['talk_word_count'])
+	else:
+		talk_overview = min(word_count_overview, key=lambda talk: talk['talk_word_count'])
+	talk_titles = [talk['talk_title'] for talk in word_count_overview if talk['talk_word_count'] == talk_overview['talk_word_count']]
+	talk_ids = [talk['talk_id'] for talk in word_count_overview if talk['talk_word_count'] == talk_overview['talk_word_count']]
+	return talk_titles, talk_ids, mean_talk_length
 
-# Create a python scipt which give you the following information:
-#   What is the longest talk (in terms of word count), what is the shortest talk (in terms of word count), what is the average word count? (id and title, numbers) (find_length)
-#   Oldest and latest talk (id and title, dates) (find_date)
-#   Is there a speaker with multiple talks? (function: find_speaker)
-#   How many English talks are there in total? (No function required, you can simply use a built-in function on the list of all English talk elements.)
 
-# find_wc:
-#   input: list of all talk elements (positional), length (longest/shortest, keyword argument)
-#   output: title(s), id(s), mean word count
+def find_date(talks, time='latest'):
+	"""
+
+	:param talks:
+	:param time:
+	:return:
+	"""
+	keyword_latest = 'latest'
+	keyword_oldest = 'oldest'
+	assert time in [keyword_oldest, keyword_latest],\
+		f"Please specify a valid time requirement, possible options are '{keyword_latest}' and '{keyword_oldest}'!"
+	datetime_overview = get_datetime_overview(talks)
+	if time == 'latest':
+		talk_overview = max(datetime_overview, key=lambda talk: talk['talk_datetime'])
+	else:
+		talk_overview = min(datetime_overview, key=lambda talk: talk['talk_datetime'])
+	talk_titles = [talk['talk_title'] for talk in datetime_overview if talk['talk_datetime'] == talk_overview['talk_datetime']]
+	talk_ids = [talk['talk_id'] for talk in datetime_overview if talk['talk_datetime'] == talk_overview['talk_datetime']]
+	return talk_titles, talk_ids
+
+
 # find_date:
-#   input: list of all talk elements (positional), time (latest/oldest, keyword argument)
-#   output: title(s), id(s)
-# find_speaker:
-#   input: list of talk elemenets (position)
-#   output: dict mapping speakers with more than one talk to their talks (tuple of talk title and id)
+# 	input: list of all talk elements (positional), time (latest/oldest, keyword argument)
+# 	output: title(s), id(s)
 
-# The total number of English talks is: [total number]
-#
-# Talk length:
-# Longest talk: [title] (id: [id])
-# Shortest talk: [title] (id: [id])
-# Mean word count: [mean word count]
+
 if __name__ == '__main__':
 	# Load the english XML file into memory and find all talks
 	path_ted_xml_english = '../Data/ted-talks/XML_releases/xml/ted_en-20160408.xml'
-	tree = et.parse(path_ted_xml_english)
-	root = tree.getroot()
-	talks = root.findall('file')
-	titles, ids, mean_word_count = find_wc(talks, length='shortest')
+	root = load_root(path_ted_xml_english)
+	talks = get_talks(root)
+
+	print(f'The total number of English talks is: {len(talks)}\n')
+	print(f'Talk length:')
+
+	titles_longest, ids_longest, mean_word_count = find_wc(talks, length='longest')
+	print(f'Longest talk(s): {titles_longest} (id(s): {ids_longest})')
+
+	titles_shortest, ids_shortest, _ = find_wc(talks, length='shortest')
+	print(f'Shortest talk(s): {titles_shortest} (id(s): {ids_shortest})')
+	print(f'Mean word count: {mean_word_count}\n')
+
+	titles_latest, ids_latest = find_date(talks, time='latest')
+	print(f'Latest talk(s): {titles_latest} (id(s): {ids_latest})')
+
+	titles_oldest, ids_oldest = find_date(talks, time='oldest')
+	print(f'Oldest talk(s): {titles_oldest} (id(s): {ids_oldest})')
--- Assignments/ASSIGNMENT_4_Max_Faber/utils.py
+++ Assignments/ASSIGNMENT_4_Max_Faber/utils.py
@@ -1,36 +1,133 @@
-def get_talk_content_text(talk_object):
+from lxml import etree as et
+from datetime import datetime
+
+import nltk
+
+
+def load_root(path):
+	tree = et.parse(path)
+	root = tree.getroot()
+	return root
+
+
+def get_talks(root):
+	talks = root.findall('file')
+	return talks
+
+
+def get_talk_id(talk):
 	"""
-	Gets the content text of a Ted's talk
-	:param talk_object: lxml element which contains the information about the talk
+
+	:param talk:
 	:return:
 	"""
-	talk_content = talk_object.find('content')
+	talk_id_str = talk.get('id')
+	talk_id_int = int(talk_id_str)
+
+	return talk_id_int
+
+
+def get_talk_title(talk):
+	"""
+
+	:param talk:
+	:return:
+	"""
+	talk_head = talk.find('head')
+	talk_title = talk_head.find('title')
+	talk_title_text = talk_title.text
+
+	return talk_title_text
+
+
+def get_word_token_count_talk(talk):
+	"""
+
+	:param talk: lxml element which contains the information about the talk
+	:return:
+	"""
+	talk_content = talk.find('content')
 	talk_text = talk_content.text
-	return talk_text
+	talk_word_tokens = nltk.word_tokenize(talk_text)
+	talk_word_token_count = len(talk_word_tokens)
+	return talk_word_token_count
 
 
-def find_shortest_talk(talks):
+def get_word_count_overview(talks):
 	"""
 
 	:param talks:
 	:return:
 	"""
-	pass
+	talk_overview_list = []
+
+	for talk in talks:
+		talk_id = get_talk_id(talk)
+		talk_title = get_talk_title(talk)
+		talk_word_count = get_word_token_count_talk(talk)
+
+		talk_overview_list.append({
+			'talk_id': talk_id,
+			'talk_title': talk_title,
+			'talk_word_count': talk_word_count
+		})
+
+	return talk_overview_list
 
 
-def find_longest_talk(talks):
+def get_mean_talk_length(talk_overview_list):
 	"""
 
 	:param talks:
 	:return:
 	"""
-	pass
+	n_words_talks = []
+
+	for talk_overview in talk_overview_list:
+		n_words_talk = talk_overview['talk_word_count']
+
+		n_words_talks.append(n_words_talk)
+	return sum(n_words_talks) / len(n_words_talks)
 
 
-def find_mean_talk_length(talks):
+def get_talk_datetime(talk):
+	"""
+
+	:param talk:
+	:return:
+	"""
+	talk_head = talk.find('head')
+	talk_date = talk_head.find('date')
+	talk_date_text = talk_date.text
+	talk_date_object = datetime.strptime(talk_date_text, '%Y/%m/%d')
+	return talk_date_object
+
+
+def get_datetime_overview(talks):
 	"""
 
 	:param talks:
 	:return:
 	"""
-	pass
+	talk_datetime_overview = []
+
+	for talk in talks:
+		talk_id = get_talk_id(talk)
+		talk_title = get_talk_title(talk)
+		talk_datetime = get_talk_datetime(talk)
+
+		talk_datetime_overview.append({
+			'talk_id': talk_id,
+			'talk_title': talk_title,
+			'talk_datetime': talk_datetime
+		})
+	return talk_datetime_overview
+
+
+
+# Remove this if not longer necessary, only for testing
+# if __name__ == '__main__':
+	# path_ted_xml_english = '../Data/ted-talks/XML_releases/xml/ted_en-20160408.xml'
+	# root = load_root(path_ted_xml_english)
+	# talks = get_talks(root)
+	# print(get_datetime(talks[0]))
