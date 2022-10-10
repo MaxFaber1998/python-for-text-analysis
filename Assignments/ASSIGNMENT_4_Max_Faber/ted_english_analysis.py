8e1a9044ddf927fcee9e13d021da2128568b5801
maxfaber <maxfaber@quicknet.nl>
2022-10-10 13:32:42 +0200

    Finished part I of assignment 4B (MA)

--- Assignments/ASSIGNMENT_4_Max_Faber/ted_english_analysis.py
+++ Assignments/ASSIGNMENT_4_Max_Faber/ted_english_analysis.py
@@ -1,4 +1,4 @@
-from utils import load_root, get_talks, get_talk_id, get_talk_title, get_talk_speaker, get_word_count_talk, get_talk_datetime, print_talk_info_list
+from utils import load_root, get_talks, get_talk_id, get_talk_title, get_talk_speaker, get_talk_word_count, get_talk_datetime, print_talk_info_list
 
 
 def find_wc(talks, length='longest'):
@@ -14,51 +14,35 @@ def find_wc(talks, length='longest'):
 	assert length in [keyword_longest, keyword_shortest],\
 		f"Please specify a valid length requirement, possible options are '{keyword_longest}' and '{keyword_shortest}'!"
 
-	# Define a list in which an overview of the word counts per talk are going to be stored
-	word_count_overview = []
-	# Define a variable which in which the sum of the word counts is going to be stored, so that the mean can be calculated
-	word_count_sum = 0
+	# As the word count per talk is an expensive function because of nltk.word_tokenize()
+	# and is either required for determining the mean word count as the longest/shortest talk,
+	# We construct a list of tuples which has the talk object as its first element
+	# and its corresponding word count as its second element
+	# This list can then be utilized for both aforementioned tasks
+	talk_word_counts = [(talk, get_talk_word_count(talk)) for talk in talks]
 
-	for talk in talks:
-		# Get the necessary information from the talk object to construct an object
-		talk_id = get_talk_id(talk)
-		talk_title = get_talk_title(talk)
-		talk_word_count = get_word_count_talk(talk)
-
-		# Construct an object containing all necessary information about the current talk and append it to the list
-		word_count_overview.append({
-			'talk_id': talk_id,
-			'talk_title': talk_title,
-			'talk_word_count': talk_word_count
-		})
-		# Increment the word count sum with the current word count
-		word_count_sum += talk_word_count
-
-	# Calculate the mean talk length
-	mean_talk_length = word_count_sum / len(talks)
+	# Define a list in which the word counts of all talks are being stored
+	word_counts = [talk_word_count[1] for talk_word_count in talk_word_counts]
+	# Calculate the mean talk length based on this list
+	mean_talk_length = sum(word_counts) / len(word_counts)
 	# Cast it to an integer
 	mean_talk_length = int(mean_talk_length)
 
 	if length == 'longest':
-		# Get the word count overview dictionary with the largest value for the key 'talk_word_count'
-		selected_word_count_overview = max(word_count_overview, key=lambda talk: talk['talk_word_count'])
+		# Get the talk/word_count tuple with the largest value for the word count (second element of the tuple)
+		selected_talk = max(talk_word_counts, key=lambda talk: talk[1])
 	else: # If the length keyword is not 'longest', it must be 'shortest' as the assertion check above has been passed
-		# Get the word count overview dictionary with the smallest value for the key 'talk_word_count'
-		selected_word_count_overview = min(word_count_overview, key=lambda talk: talk['talk_word_count'])
+		# Get the talk/word_count tuple with the smallest value for the word count (second element of the tuple)
+		selected_talk = min(talk_word_counts, key=lambda talk: talk[1])
 
-	# Define two lists called 'talk_titles' and 'talk_ids', which are going to be the return values after adding the corresponding value(s)
-	talk_titles = []
-	talk_ids = []
-	for word_count_talk in word_count_overview:
-		# Find all word count overview dictionaries with the word count stored in 'selected_word_count_overview'
-		if word_count_talk['talk_word_count'] == selected_word_count_overview['talk_word_count']:
-			# Store the talk title and talk id in a variable
-			talk_title = word_count_talk['talk_title']
-			talk_id = word_count_talk['talk_id']
+	# Construct a list of all talk/word_count tuples with the word count equal to the word count in 'selected_talk' (second element of both tuples)
+	# This is necessary as it's possible that there's multiple talks with this word count
+	# The min() and max() functions only return one talk, even if there's multiple talks that satisfy this same requirement
+	selected_talks = [talk for talk in talk_word_counts if talk[1] == selected_talk[1]]
+	# Construct a list of all title(s) and a list of all id(s)
+	talk_titles = [get_talk_title(talk[0]) for talk in selected_talks]
+	talk_ids = [get_talk_id(talk[0]) for talk in selected_talks]
 
-			# Append the variables to the lists
-			talk_titles.append(talk_title)
-			talk_ids.append(talk_id)
 	# Return the tuple of talk title(s), talk id(s) and the mean talk length
 	return talk_titles, talk_ids, mean_talk_length
 
@@ -76,42 +60,20 @@ def find_date(talks, time='latest'):
 	assert time in [keyword_oldest, keyword_latest],\
 		f"Please specify a valid time requirement, possible options are '{keyword_latest}' and '{keyword_oldest}'!"
 
-	# Define a list in which an overview of the datetime objects per talk are going to be stored
-	datetime_overview = []
-
-	for talk in talks:
-		# Get the necessary information from the talk object to construct an object
-		talk_id = get_talk_id(talk)
-		talk_title = get_talk_title(talk)
-		talk_datetime = get_talk_datetime(talk)
-
-		# Construct an object containing all necessary information about the current talk and append it to the list
-		datetime_overview.append({
-			'talk_id': talk_id,
-			'talk_title': talk_title,
-			'talk_datetime': talk_datetime
-		})
-
 	if time == 'latest':
-		# Get the datetime overview dictionary with the largest value for the key 'talk_datetime'
-		selected_datetime_overview = max(datetime_overview, key=lambda talk: talk['talk_datetime'])
+		# Get the talk with the latest datetime
+		selected_talk = max(talks, key=lambda talk: get_talk_datetime(talk))
 	else: # If the time keyword is not 'latest', it must be 'oldest' as the assertion check above has been passed
-		# Get the datetime overview dictionary with the smallest value for the key 'talk_datetime'
-		selected_datetime_overview = min(datetime_overview, key=lambda talk: talk['talk_datetime'])
+		# Get the talk with the oldest datetime
+		selected_talk = min(talks, key=lambda talk: get_talk_datetime(talk))
 
-	# Define two lists called 'talk_titles' and 'talk_ids', which are going to be the return values after adding the corresponding value(s)
-	talk_titles = []
-	talk_ids = []
-	for datetime_talk in datetime_overview:
-		# Find all datetime overview dictionaries with the word count stored in 'selected_datetime_overview'
-		if datetime_talk['talk_datetime'] == selected_datetime_overview['talk_datetime']:
-			# Store the talk title and talk id in a variable
-			talk_title = datetime_talk['talk_title']
-			talk_id = datetime_talk['talk_id']
+	# Construct a list of all title(s) and a list of all id(s)
+	# It's necessary to search for all talk objects with the datetime stored in 'selected_talk', as it's possible that
+	# there's multiple talks with this datetime
+	# The min() and max() functions only return one talk, even if there's multiple talks that satisfy this same requirement
+	talk_titles = [get_talk_title(talk) for talk in talks if get_talk_datetime(talk) == get_talk_datetime(selected_talk)]
+	talk_ids = [get_talk_id(talk) for talk in talks if get_talk_datetime(talk) == get_talk_datetime(selected_talk)]
 
-			# Append the variables to the lists
-			talk_titles.append(talk_title)
-			talk_ids.append(talk_id)
 	# Return the tuple of talk title(s) and the talk id(s)
 	return talk_titles, talk_ids
 
@@ -162,29 +124,36 @@ if __name__ == '__main__':
 
 	# Get and print longest title(s) and id(s) of the longest talk
 	print('Talk length:')
-	print('Longest talk(s):')
 	titles_longest, ids_longest, mean_word_count = find_wc(talks, length='longest')
+	print('Longest talk(s):')
 	print_talk_info_list(titles_longest, ids_longest)
 
 	# Get and print longest title(s) and id(s) of the shortest talk
-	print('Shortest talk(s):')
 	titles_shortest, ids_shortest, _ = find_wc(talks, length='shortest')
+	print('Shortest talk(s):')
 	print_talk_info_list(titles_shortest, ids_shortest)
 
+	# Print the mean word count
 	print(f'Mean word count: {mean_word_count}\n')
 
-	print('Latest talk(s):')
+	# Get and print the title(s) and id(s) of the latest talk(s)
 	titles_latest, ids_latest = find_date(talks, time='latest')
+	print('Latest talk(s):')
 	print_talk_info_list(titles_latest, ids_latest)
 
-	print('Oldest talk(s)')
+	# Get and print the title(s) and id(s) of the oldest talk(s)
 	titles_oldest, ids_oldest = find_date(talks, time='oldest')
+	print('Oldest talk(s)')
+	print_talk_info_list(titles_oldest, ids_oldest)
 
 	print()
 
-	print('Speaker(s) with multiple talks:')
+	# Get and print the title(s) and id(s) of the speakers which gave multiple talks
 	multiple_talk_speakers = find_speaker(talks)
+	print('Speaker(s) with multiple talks:')
 	for speaker, talk_info_tuple_list in multiple_talk_speakers.items():
+		# Construct lists of the id(s) (first element) and title(s) (second element) separately using the list of tuples
+		# We need this data format in order to utilize the utility function 'print_talk_info_list()'
 		talk_ids = [talk_info_tuple[0] for talk_info_tuple in talk_info_tuple_list]
 		talk_titles = [talk_info_tuple[1] for talk_info_tuple in talk_info_tuple_list]
 
--- Assignments/ASSIGNMENT_4_Max_Faber/utils.py
+++ Assignments/ASSIGNMENT_4_Max_Faber/utils.py
@@ -5,93 +5,130 @@ import nltk
 
 
 def load_root(path):
+	"""
+	Load the root of the XML file
+	:param path: Path of the XML file
+	:return: Root object of the XML file
+	"""
 	tree = et.parse(path)
 	root = tree.getroot()
 	return root
 
 
 def get_talks(root):
+	"""
+	Get all talks af the root object of an XML object
+	:param root: Root object
+	:return: List of file (talk) objects
+	"""
 	talks = root.findall('file')
 	return talks
 
 
 def get_talk_id(talk):
 	"""
-
-	:param talk:
-	:return:
+	Gets the id of a talk
+	:param talk: Talk object
+	:return: The talk id (integer)
 	"""
+	# Get the head of the talk object
 	talk_head = talk.find('head')
+	# Find the talk id object in the talk head object
 	talk_id = talk_head.find('talkid')
+	# Extract the id (string) from the talk id object
 	talk_id_str = talk_id.text
+	# Cast the talk id (string) to an integer
 	talk_id_int = int(talk_id_str)
 	return talk_id_int
 
 
 def get_talk_title(talk):
 	"""
-
-	:param talk:
-	:return:
+	Get the title of the given talk
+	:param talk: Talk object
+	:return: The title of the talk (string)
 	"""
+	# Get the head object of the talk object
 	talk_head = talk.find('head')
+	# Find the title of the talk object
 	talk_title = talk_head.find('title')
+	# Extract the title (string) from the head object of the talk object
 	talk_title_text = talk_title.text
 	return talk_title_text
 
 
-def get_word_count_talk(talk):
+def get_talk_word_count(talk):
 	"""
-
-	:param talk: lxml element which contains the information about the talk
-	:return:
+	Get the no. words of the given talk
+	:param talk: Talk object
+	:return: The no. words spoken during the talk (integer)
 	"""
+	# Find an object containing the content of the talk
 	talk_content = talk.find('content')
+	# Extract the text from the talk content oject
 	talk_text = talk_content.text
+	# Tokenize the words of the talk content using the nltk package
 	talk_word_tokens = nltk.word_tokenize(talk_text)
+	# Get the no. tokens
 	talk_word_token_count = len(talk_word_tokens)
 	return talk_word_token_count
 
 
 def get_talk_speaker(talk):
 	"""
-
-	:param talk:
-	:return:
+	Get the speaker of the given talk
+	:param talk: Talk object
+	:return: The speaker of the talk (string)
 	"""
+	# Find the head object of the talk object
 	talk_head = talk.find('head')
+	# Find the speaker object of the head object
 	talk_speaker = talk_head.find('speaker')
+	# Extract the speaker (string) from the speaker object
 	talk_speaker_text = talk_speaker.text
 	return talk_speaker_text
 
 
 def get_talk_datetime(talk):
 	"""
-
-	:param talk:
-	:return:
+	Get a datetime object of which the given talk was given
+	:param talk: Talk object
+	:return: Datetime of the given talk
 	"""
+	# Find the head object of the given talk object
 	talk_head = talk.find('head')
+	# Find the date object of the head object
 	talk_date = talk_head.find('date')
+	# Extract the date (string) from the date object
 	talk_date_text = talk_date.text
+	# Convert the date (string) to a datetime object ('%Y/%m/%d' indicates the format, for example: 2005/04/22)
 	talk_date_object = datetime.strptime(talk_date_text, '%Y/%m/%d')
 	return talk_date_object
 
 
 def print_talk_info_list(talk_titles, talk_ids):
 	"""
-
-	:param talk_titles:
-	:param talk_ids:
-	:return:
+	Prints an indented overview of the given titles and ids
+	This function has been written as it's utilized multiple times and will clean up the code in 'ted_english_analysis.py'
+	:param talk_titles: A list of titles to be printed
+	:param talk_ids: A list of ids to be printed
+	:return: None
 	"""
+	# Zip the titles and ids together, as they're in the same order
 	talk_info = zip(talk_titles, talk_ids)
+	# Convert the zip object to a list of tuples (titles: first element, ids: second element)
 	talk_info = list(talk_info)
 
+	# Iterate over the list of tuples with titles and ids
 	for index, talk_tuple in enumerate(talk_info):
+		# Extract the current title and id from the tuple
 		talk_title, talk_id = talk_tuple
 
+		# Print the title with it's corresponding id with a tab at the start in order to improve readability
 		print(f"\t'{talk_title}' (id: {talk_id})", end='')
+		# Check if the current iteration is not the latest one
 		if index < len(talk_info) - 1:
-			print(', ')
+			# If not, print a comma to showcase it's not the latest element in the output
+			print(',')
+	# Print a period to showcase the end of the overview
 	print('.')
